0813c3880ab404405554faecba12dbc6f3462a95
willdupree90 <willdupree90@gmail.com>
2024-09-21 19:02:05 -0600

    Addressed #2, where we have a first version of adding projects from git. Repo url, access token, and branch name can be set.

--- src/edoc/chatbot.py
+++ src/edoc/chatbot.py
@@ -1,6 +1,6 @@
 import gradio as gr
 from edoc.gpt_helpers.connect import OpenAiConfig
-from edoc.chatbot_components.utils import response, set_openai_api_key, capture_directory_path, delete_graph_data
+from edoc.chatbot_components.utils import response, set_openai_api_key, create_graph_from_zip, create_graph_from_git, delete_graph_data
 
 OPENAI_API_KEY = OpenAiConfig.get_openai_api_key()
 
@@ -25,21 +25,32 @@ with gr.Blocks(fill_height=True) as demo:
                 set_api_key_button.click(set_openai_api_key, inputs=api_key_input, outputs=api_key_output)
 
         with gr.Tab("Upload files"):
-            # dir_input = gr.Textbox(label="Enter Directory Path (Warning: Submitting will process files through LLM and can take time).", placeholder="Enter or paste the path to your directory here...")
-            # submit_button = gr.Button("Submit")
-            upload_button = gr.UploadButton(label="Upload Your Zipped Code Files", file_types=[".zip"], file_count="single")
-            output = gr.Textbox(label="Graph Progress")
-            upload_button.upload(capture_directory_path, upload_button, output)
-            
+            gr.Markdown("## Choose Your Method to Upload Code Files")
 
-            # submit_button.click(capture_directory_path, inputs=dir_input, outputs=output)
+            upload_output = gr.Textbox(label="Graph Progress")
+
+            with gr.Group():
+                gr.Markdown("### Upload Your Zipped Code Files")
+                upload_zip_button = gr.UploadButton(label="Select ZIP File", file_types=[".zip"], file_count="single")
+                upload_zip_button.upload(create_graph_from_zip, upload_zip_button, upload_output)
+
+            with gr.Group():
+                gr.Markdown("### Import Your Git Project via URL")
+                with gr.Row():
+                    upload_git_input = gr.Textbox(label="Upload Your Git Project via URL")
+                    set_access_token = gr.Textbox(label="Set Access Token (Optional)")
+                    set_branch = gr.Textbox(label="Set Git Branch (if not 'main')")
+
+                upload_git_button = gr.Button("Import Git Project")
+
+                upload_git_button.click(create_graph_from_git, [upload_git_input, set_access_token, set_branch], upload_output) 
 
         with gr.Tab("Delete data"):
             keyword_input = gr.Textbox(label="Please enter 'Delete' to remove data.")
             submit_button = gr.Button("Submit")
 
-            output = gr.Textbox(label="Output")
-            submit_button.click(delete_graph_data, inputs=keyword_input, outputs=output)
+            delete_output = gr.Textbox(label="Output")
+            submit_button.click(delete_graph_data, inputs=keyword_input, outputs=delete_output)
 
 if __name__ == "__main__":
     demo.launch(share=True)
--- src/edoc/chatbot_components/utils.py
+++ src/edoc/chatbot_components/utils.py
@@ -3,6 +3,8 @@ import gradio as gr
 import os
 import zipfile
 
+from git import Repo, GitCommandError
+
 from edoc.gpt_helpers.gpt_basics import get_embedding
 from edoc.rag_components.responder import BuildResponse
 from edoc.kg_construction.bulk_load import CodebaseGraph
@@ -20,6 +22,18 @@ if OPENAI_API_KEY is not None:
 kg = connect_to_neo4j()
 
 def set_openai_api_key(api_key):
+    """
+    Set the OpenAI API key dynamically.
+
+    This function attempts to set the OpenAI API key and tests the key by performing 
+    an embedding call. If the key is invalid, an error message is returned.
+
+    Args:
+        api_key (str): The OpenAI API key provided by the user.
+
+    Returns:
+        str: A success message if the key is valid, or an error message if the key is invalid.
+    """
     global api_key_set
     try:
         # Set the new API key
@@ -35,6 +49,20 @@ def set_openai_api_key(api_key):
         return f"An error occurred while testing the API key: {e}"
 
 def response(message, history):
+    """
+    Generate a chatbot response based on user input and chat history.
+
+    This function uses a pre-built responder model to get a response based on the user's
+    question, using a knowledge graph for context. It returns an error message if the 
+    OpenAI API key has not been set.
+
+    Args:
+        message (str): The user's question.
+        history (list): The chat history up to this point.
+
+    Returns:
+        str: The chatbot's response or an error message.
+    """
     if not api_key_set:
         return "Error: Please provide an OpenAI API key in `Manage` dropdown before using the chatbot."
 
@@ -51,6 +79,19 @@ def response(message, history):
         return f"An error occurred while processing your request: {e}."
 
 def get_project_root_from_temp_location(zip_file):
+    """
+    Extract the contents of a ZIP file and return the path to the extracted directory.
+
+    This function extracts a ZIP file into a designated local directory, ensuring the 
+    structure is cross-platform compatible. It expects that the ZIP contains a single 
+    root directory.
+
+    Args:
+        zip_file (file): The ZIP file to be extracted.
+
+    Returns:
+        str: The path to the extracted project directory or None if an error occurs.
+    """
     # Create an upload directory inside your system
     upload_dir = os.path.normpath("../../edocSourceData")  # Local directory for testing
 
@@ -60,7 +101,7 @@ def get_project_root_from_temp_location(zip_file):
         with zipfile.ZipFile(zip_file.name, 'r') as zip_ref:
             zip_ref.extractall(upload_dir)
     except Exception as e:
-        return f"Error extracting zip file: {e}"
+        print(f"Error extracting zip file: {e}")
 
     # Use os.path to handle cross-platform path splitting
     root_name = os.path.basename(zip_file.name)  # Get the file name
@@ -72,9 +113,20 @@ def get_project_root_from_temp_location(zip_file):
     
     return extracted_project_root
     
-    
-def capture_directory_path(zip_file, progress=gr.Progress(track_tqdm=True)):
+def create_graph_from_zip(zip_file, progress=gr.Progress(track_tqdm=True)):
+    """
+    Create a knowledge graph from a ZIP file.
 
+    This function extracts the ZIP file, processes the codebase, and generates 
+    a knowledge graph. If the API key is not set, an error is returned.
+
+    Args:
+        zip_file (file): The ZIP file containing the codebase.
+        progress (gr.Progress, optional): Gradio's progress tracker.
+
+    Returns:
+        str: Success message or an error message.
+    """
     if not api_key_set:
         return "Error: Please provide an OpenAI API key in `Manage` dropdown before using the chatbot."
 
@@ -87,8 +139,98 @@ def capture_directory_path(zip_file, progress=gr.Progress(track_tqdm=True)):
     
     return "Could not successfully read file from zip. \nEnsure the ZIP file contains a single root directory (top-level folder) that shares the ZIP's name. All other files, folders, and subdirectories are then placed inside that root directory."
 
+def get_project_root_from_github(repo_url, git_token=None, use_branch=None):
+    """
+    Clone a GitHub repository and return the path to the project root.
+    
+    Args:
+        repo_url (str): The URL of the GitHub repository to clone.
+        git_token (str, optional): GitHub Personal Access Token for private repos.
+        use_branch (str): The branch to clone (default is 'main').
+        
+    Returns:
+        str: Path to the root of the cloned project or none.
+    """
+    # Create an upload directory inside your system
+    upload_dir = os.path.normpath("../../edocSourceData")  # Local directory for testing
+
+    os.makedirs(upload_dir, exist_ok=True)  # Ensure the directory exists
+
+    # If a token is provided, modify the repo URL to include the token for authentication
+    if git_token:
+        # Format the repo URL with the token
+        repo_url = repo_url.replace("https://", f"https://{git_token}@")
+
+    # Extract the repo name from the URL to use as the folder name
+    repo_name = os.path.splitext(os.path.basename(repo_url))[0]
+    project_dir = os.path.join(upload_dir, repo_name)
+
+    branch = 'main'
+    if use_branch:
+        branch = use_branch
+
+    try:
+        # Clone the repo into the specified directory
+        Repo.clone_from(repo_url, project_dir, branch=branch)
+    except GitCommandError as e:
+        if "Authentication failed" in str(e):
+            print("Error: Authentication failed. Please check your GitHub token.")
+        else:
+            print(f"Error cloning GitHub repository: {e}")
+            return None
+    except Exception as e:
+        print(f"Error cloning GitHub repository: {e}")
+        return None
+
+    # Return the project directory path
+    if not os.path.exists(project_dir):
+        return None
+    
+    return project_dir
+
+def create_graph_from_git(git_url, git_token=None, use_branch=None, progress=gr.Progress(track_tqdm=True)):
+    """
+    Create a knowledge graph from a GitHub repository.
+
+    This function clones a GitHub repository, processes the codebase, and generates 
+    a knowledge graph. If the API key is not set, an error is returned.
+
+    Args:
+        git_url (str): The URL of the GitHub repository.
+        git_token (str, optional): GitHub Personal Access Token for private repos.
+        use_branch (str): The branch to clone (default is 'main').
+        progress (gr.Progress, optional): Gradio's progress tracker.
+
+    Returns:
+        str: Success message or an error message.
+    """
+    if not api_key_set:
+        return "Error: Please provide an OpenAI API key in `Manage` dropdown before using the chatbot."
+
+    root_dir = get_project_root_from_github(git_url, git_token, use_branch)
+    if root_dir is not None:
+        codebase_graph = CodebaseGraph(root_directory=root_dir)
+        codebase_graph.create_graph()
+
+        return "Successfully created graph from Git project."
+    
+    return "Could not successfully clone Git, did not extract to graph."
+
 # Function to delete graph data
+
 def delete_graph_data(keyword):
+    """
+    Delete all nodes and relationships from the knowledge graph.
+
+    This function clears the entire knowledge graph by running a `MATCH (n) DETACH DELETE n`
+    Cypher query if the correct keyword is provided.
+
+    Args:
+        keyword (str): The keyword to trigger the deletion. Must be 'Delete'.
+
+    Returns:
+        str: Success message or an error message.
+    """
     magic_keyword = "Delete"
     if keyword == magic_keyword:  # Example dangerous keyword
         try:
